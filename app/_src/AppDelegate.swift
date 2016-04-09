6dc9fbb4dd69c19713821e9d2623d0c4151996cd
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2016-04-09 12:04:21 -0700

    chore (code) refactor app delegate to use extensions
    isolate individual components and keep the app delegate super lightweight

--- app/_src/AppDelegate.swift
+++ app/_src/AppDelegate.swift
@@ -8,61 +8,12 @@
 
 import UIKit
 
-//NOTE: Create global 'log' instance
-//      this allows, more refined logging
-//      based on build - set log level (verbose, warn, error, etc)
-let log = XCGLogger.defaultInstance()
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
-        setupLogging()
-        registerDefaults()
-        
-        setupApplicationDocuments()
-        
+    func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        setup()
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {}
-
-    func applicationDidEnterBackground(application: UIApplication) {}
-
-    func applicationWillEnterForeground(application: UIApplication) {}
-
-    func applicationDidBecomeActive(application: UIApplication) {}
-
-    func applicationWillTerminate(application: UIApplication) {}
-    
-    //MARK: Setup
-    private func setupApplicationDocuments() {
-        
-        //NOTE: Move any resource bundle files to `Documents`
-        //      directory, etc
-        
-        let mainBundle = NSBundle.mainBundle()
-        if let path = mainBundle.pathForResource(CastleChallengeFiles.DefaultData.FileName, ofType: CastleChallengeFiles.DefaultData.FileExtension) {
-            
-            FileManager.defaultManager.copyFileToApplicationDocumentsDir(path)
-        }
-    }
-    
-    private func setupLogging() {
-        
-        //TODO: Change Log Level based on Environment (Debug -> Verbose / Release -> Warning)
-        log.setup(.Verbose, showLogLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: nil, fileLogLevel: nil)
-        
-        //NOTE: Logs can be sent to a file...
-        //log.setup(.Error, showLogLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: nil, fileLogLevel: nil)
-    }
-    
-    //MARK: App & User Defaults
-    private func registerDefaults() {
-        UserDefaults.defaultManager.setup()
-    }
 }
-
