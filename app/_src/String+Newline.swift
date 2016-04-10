0c24b45e5a188d6c01693749850f138aacc79d8b
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2016-04-09 20:20:14 -0700

    feat (code) newline extension for String

---
+++ app/_src/String+Newline.swift
@@ -0,0 +1,20 @@
+//
+//  String+Newline.swift
+//  MC Castle Challenge
+//
+//  Created by Gregory Hutchinson on 4/9/16.
+//  Copyright © 2016 Gregory Hutchinson. All rights reserved.
+//
+
+import Foundation
+
+/** Newline Extends String
+
+*/
+extension String {
+    static var newline: String {
+        get {
+            return "/n"
+        }
+    }
+}
